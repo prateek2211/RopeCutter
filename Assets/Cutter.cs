e35ba277647de23dd72ced5ae9bd5084061b17d2
Prateek Sachan <psachan@cs.iitr.ac.in>
2019-04-07 10:22:46 +0530

    Intitial commit

---
+++ Assets/Cutter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutter : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit2D raycast = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (raycast.collider)
+            {
+                if (raycast.collider.tag.Equals("Link"))
+                {
+                    Destroy(raycast.collider.gameObject);
+                };
+            }
+        }
+    }
+}
---
+++ Assets/RopeGenerator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class RopeGenerator : MonoBehaviour
+{
+    public int links;
+    public GameObject link;
+    public Rigidbody2D previousLink;
+    public GameObject weight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < links; i++)
+        {
+            GameObject currentLink = Instantiate(link, transform);
+            currentLink.GetComponent<HingeJoint2D>().connectedBody = previousLink;
+            previousLink = currentLink.GetComponent<Rigidbody2D>();
+        }
+        HingeJoint2D hinge = weight.AddComponent<HingeJoint2D>();
+        hinge.autoConfigureConnectedAnchor = false;
+        hinge.anchor = Vector2.zero;
+        hinge.connectedAnchor = new Vector2(0.0f, -0.6f);
+        hinge.connectedBody = previousLink;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
